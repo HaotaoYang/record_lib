4fc425992e46c9bf120e2051e9e82ac0edaf59ab
Holtom <520023290@qq.com>
2021-01-07 23:00:50 +0800

    update

--- src/record_lib.erl
+++ src/record_lib.erl
@@ -10,26 +10,35 @@
 %%% ============================================================================
 %%% API
 %%% ============================================================================
-map_2_record(RecordName, Map) ->
-    case erlang:apply(?RECORD_UTIL_MOD, get_record, [RecordName]) of
+map_2_record(Map, RecordName) when is_atom(RecordName) ->
+    case get_record(RecordName) of
         undefined -> undefined;
-        Record ->
-            Values = tuple_to_list(erlang:delete_element(1, Record)),
-            MapKeys = maps:keys(Map),
-            MapValues = maps:values(Map),
-            RecordFields = erlang:apply(?RECORD_UTIL_MOD, fields_info, [RecordName]),
-            FieldsValue = [
-                begin
-                    case get_field_index(Field, MapKeys) of
-                        N when is_integer(N) ->
-                            lists:nth(N, MapValues);
-                        _ ->
-                            lists:nth(Index, Values)
-                    end
-                end || {Index, Field} <- lists:zip(lists:seq(1, length(RecordFields)), RecordFields)
-            ],
-            list_to_tuple([RecordName | FieldsValue])
+        Record -> do_map_2_record(Map, Record)
+    end;
+map_2_record(Map, Record) when is_tuple(Record) ->
+    RecordName = element(1, Record),
+    case is_record(Record, RecordName) andalso is_record(RecordName) of
+        true -> do_map_2_record(Map, Record);
+        _ -> undefined
     end.
+
+do_map_2_record(Map, Record) ->
+    RecordName = element(1, Record),
+    Values = tuple_to_list(erlang:delete_element(1, Record)),
+    MapKeys = maps:keys(Map),
+    MapValues = maps:values(Map),
+    RecordFields = fields_info(RecordName),
+    FieldsValue = [
+        begin
+            case get_field_index(Field, MapKeys) of
+                N when is_integer(N) ->
+                    lists:nth(N, MapValues);
+                _ ->
+                    lists:nth(Index, Values)
+            end
+        end || {Index, Field} <- lists:zip(lists:seq(1, length(RecordFields)), RecordFields)
+    ],
+    list_to_tuple([RecordName | FieldsValue]).
 
 get_field_index(Field, Keys) ->
     do_get_field_index(Field, Keys, 1).
@@ -41,13 +50,18 @@ do_get_field_index(Field, Keys, N) ->
         _ -> do_get_field_index(Field, Keys, N + 1)
     end.
 
-record_2_map(Record) ->
+record_2_map(Record) when is_tuple(Record) ->
     RecordName = element(1, Record),
-    Fields = erlang:apply(?RECORD_UTIL_MOD, fields_info, [RecordName]),
-    Values = tuple_to_list(erlang:delete_element(1, Record)),
+    case is_record(Record, RecordName) andalso is_record(RecordName) of
+        true -> do_record_2_map(RecordName, erlang:delete_element(1, Record));
+        _ -> undefined
+    end.
+
+do_record_2_map(RecordName, Values) ->
+    Fields = fields_info(RecordName),
     {RetMap, _} = lists:foldl(
         fun(Field, {TempMap, Index}) ->
-            NewMap = maps:put(Field, lists:nth(Index, Values), TempMap),
+            NewMap = maps:put(Field, lists:nth(Index, tuple_to_list(Values)), TempMap),
             {NewMap, Index + 1}
         end,
         {#{}, 1},
@@ -66,3 +80,12 @@ get_record_util_opts() ->
         {module_name, ModuleName} when is_atom(ModuleName) -> ModuleName;
         _ -> record_helper
     end.
+
+is_record(RecordName) ->
+    erlang:apply(?RECORD_UTIL_MOD, is_record, [RecordName]).
+
+get_record(RecordName) ->
+    erlang:apply(?RECORD_UTIL_MOD, get_record, [RecordName]).
+
+fields_info(RecordName) ->
+    erlang:apply(?RECORD_UTIL_MOD, fields_info, [RecordName]).
